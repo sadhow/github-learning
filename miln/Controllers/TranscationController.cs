598c21a233c95eea95c59e1e29bf1d310593d916
Manash <protimm1@gmail.com>
2024-05-17 22:49:33 +0530

    Add - Transaction Controller

---
+++ miln/Controllers/TranscationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace miln.Controllers
+{
+    public class TranscationController : Controller
+    {
+        // GET: Transcation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
